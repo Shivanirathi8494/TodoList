8a40e5ea6ebf26b65b42826233460053db39e90c
Shivani Rathi <srathi@crestron.com>
2022-07-24 17:38:10 +0530

    adding new changes, CSS files and images.

--- dbinitalizer.py
+++ dbinitalizer.py
@@ -6,7 +6,7 @@ print("Opened database todolist successfully");
 conn.execute("CREATE TABLE user (user_id INTEGER PRIMARY KEY AUTOINCREMENT,user_name TEXT NOT NULL,password TEXT NOT NULL,email TEXT NOT NULL UNIQUE)")
 print("Table user created successfully");
 
-conn.execute("CREATE TABLE todolist (user_id INTEGER,todo_item TEXT NOT NULL,status TEXT NOT NULL)")
+conn.execute("CREATE TABLE todolist (todo_item_id INTEGER PRIMARY KEY AUTOINCREMENT,user_id INTEGER,todo_item TEXT NOT NULL,status TEXT NOT NULL)")
 print("Table todolist created successfully");
 
 conn.close()
